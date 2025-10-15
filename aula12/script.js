3ee6cff50f05a3cf8948f850a2e79b9633577098
vagnerBmoura <04330458098@senacrs.edu.br>
2025-10-14 21:54:38 -0300

    adicionar a pasta de aula12

---
+++ aula12/script.js
@@ -0,0 +1,98 @@
+/*let condicao1 = true
+
+if(condicao2){
+    console.log("Entrei no if 1")
+}
+
+let condicao2 = false
+if(condicao2){
+    console.log("entrei na if 2 ")
+} 
+
+let num1 = Number(prompt("Digite o primeiro numero"))
+let num2 = Number(prompt("digite o segundo numero"))
+
+if (num1 === num2){
+    console.log("executou o if")
+    console.log("o primeiro numero e igual que o segundo")
+}
+
+else{
+console.log("executou o else")
+console.log("os numeros sao diferentes")
+}
+
+
+let condicao1= true 
+let condicao2 = true
+let condicao3 = true 
+
+
+ if(condicao1){
+    console.log("entrou no else if 1")
+
+}
+
+else if(condicao2){
+    console.log("entrou no else if 1")
+}
+
+else if(condicao3){
+    console.log("entrou no else if 2")
+}
+
+else
+console.log("entrou no else")
+
+const num1 = Number(prompt("digite o numero 1 "))
+const num2 = Number(prompt("digite o numero 2 "))
+
+const resultado = num1 === num2
+if(num1>num2){
+    console.log("numero1 e maior que o numero2")
+}
+else if (num1<num2){
+    console.log("numero 1 e menor que o numero 2 ")
+}
+else 
+console.log("os numeros sao iguais")
+
+let paisOrigem = prompt("digite o seu pais de origem")
+switch(paisOrigem){
+    case 'Brasil' :
+        console.log("nacionalidade brasileira")
+        console.log("Entrou no case1")
+        break
+        case 'EUA' :
+        console.log("nacionalidade americana")
+        console.log("Entrou na case2")
+        break
+        case `india` :
+            console.log("nacionalidade indiano")
+            console.log("Entrou na case3")
+            break
+            default : 
+            console.log("Nacionalidade nao encontrada")
+            break
+}*/
+
+let pokemon = prompt("digite seu pokemon")
+switch(pokemon){
+    case 'bubasaulr' :
+        console.log("Bulbasaulr(Planta e Veneno)")
+        console.log("entrou na case1")
+        break
+        case 'charmander' :
+            console.log("Charmander(Fogo)")
+            console.log("entrou na case2")
+            break
+            case 'squirtle' : 
+            console.log("Squirtle(Agua)")
+            console.log("entrou na case3")
+            break
+            default : 
+            console.log("pokemon nao encontrado")
+            break
+            }
+
+
