cf889c477c55fb3021061a4dc905465191349869
vagnerBmoura <04330458098@senacrs.edu.br>
2025-10-28 21:51:31 -0300

    adicionar a pasta aula15

---
+++ aula15/script.js
@@ -0,0 +1,36 @@
+/*let i = 0
+while (i<10){
+console.log(i)
+i++
+}//termino corrida laço
+
+let estomago = 0 
+while (estomago<100){
+    console.log("quero comer mais coxinhas", estomago)
+    estomago = estomago +10
+}
+
+let I
+let resultado = 0 
+const numeros = [10,3, 50 ,7 , 0]
+while (I !== 0 ){
+    i = Number(prompt("coloque um numero"))
+    resultado += I
+}
+console.log(resultado)
+
+const numeros2 = [14, 67, 89, 15, 23]
+for(let i= 0; i < 5; i++){
+    const elemento = numeros[i]
+    console.log(elemento)
+}
+*/
+let maiorNumero = 0
+const numeros1 = [11,15,18,14,12,13]
+for(let i=0; i < 6; i++){
+if(maiorNumero > numeros1[i]){
+    maiorNumero = numeros1[i]
+}
+}
+console.log(numeros1[i])
+
