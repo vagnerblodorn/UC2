0cda25496a04fedce29d0012230e1dbcebb39860
vagnerBmoura <04330458098@senacrs.edu.br>
2025-11-10 21:47:24 -0300

    adicionar a pasta aula16

--- aula16/script.js
+++ aula16/script.js
@@ -23,7 +23,7 @@ function imprimirDoisNumeros(DoisNumeros){
 }
 
 imprimirDoisNumeros("3")
-imprimirDoisNumeros("5") */
+imprimirDoisNumeros("5") 
 
 function determinaMonstro (temPeleGelada,transformaComLuaCheia,brilhaAoSol,temSedeDeSangue){
     if (brilhaAoSol&&temPeleGelada&&temSedeDeSangue){
@@ -38,6 +38,100 @@ function determinaMonstro (temPeleGelada,transformaComLuaCheia,brilhaAoSol,temSe
 
 }
 determinaMonstro(true,false,true,true)
+
+let num1 = Number(prompt("digite o num"))
+let num2 = Number(prompt("digite o num2"))
+
+function soma (num1 , num2){
+    return num1 + num2
+}
+console.log("Numeros inseridos:",num1,num2)
+console.log("soma:",soma(num1,num2))
+
+let number1 = Number(prompt("digite o number"))
+let number2 = Number(prompt("digite o number2"))
+
+function diferenca (number1 , number2){
+    return number1 + number2
+}
+console.log("Numeros inseridos:,number1, number2")
+console.log("soma:",diferenca(number1, number2))
+
+let numero1 = Number(prompt("digite o numero"))
+let numero2 = Number(prompt("digite o numero2"))
+
+function multiplicacao (numero1 , numero2){
+    return numero1 + numero2
+}
+console.log("Numeros inseridos:",numero1,numero2)
+console.log("soma:",multiplicacao(numero1,numero2))
+
+let numeros1 = Number(prompt("digite o num"))
+let numeros2 = Number(prompt("digite o num2"))
+
+function divisao (numeros1 , numeros2){
+    return numeros1 + numeros2
+}
+console.log("Numeros inseridos:",numeros1,numeros2)
+console.log("soma:",divisao(numeros1,numeros2))
+
+const professor = {
+    nome : "Maria" ,
+    idade : 28,
+    tarefas: ["Dar aula", "Responder dúvidas"],
+    contarPiada: function(){
+    console.log("É pa vê ou pa comê? ")}
+}
+
+const filme = {
+    direcao : "marcos siega " ,
+    nome : "dexter" ,
+    lancamento : 2022 ,
+    lista: ["dexter", "jennifer", "zayas" , "desmond"],
+    jaOlhei: function(){
+        console.log("Ja olhei o filme")}
+}
+
+console.log(filme.direcao)
+console.log(filme.nome)
+console.log(filme.lancamento)
+console.log(filme.lista)
+console.log(filme.jaOlhei)
+
+jaOlhei()
+
+const colega ={
+    nome:"vinicius",
+    idade: 17, 
+    musica:"funk",
+}
+console
+
+const profssores = [
+    {nome : "Lucas", modulo : 2},
+    {nome : "dal", modulo : 1 },
+    {nome : "valter", modulo : 1}
+]
+console.log("O professor:", profssores [0].nome, " deu o modulo:", profssores[0].modulo)*/
+
+
+const filme = {
+    direcao : "marcos siega " ,
+    nome : "dexter" ,
+    lancamento : 2022 ,
+    lista: ["Xuxa", "jennifer", "zayas" , "desmond"],
+    lista2: ["dexter morgan", "debra morgan", "angel batista", "joey quiin" ],
+    jaOlhei: function(){
+        console.log("Ja olhei o filme")}
+}
+
+console.log(filme.direcao)
+console.log(filme.nome)
+console.log(filme.lancamento)
+console.log(filme.lista)
+console.log(filme.lista2)
+console.log(filme.jaOlhei)
+
 
 
 
